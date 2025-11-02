1de3dc776475e2ce253e25c3da03c1f581cf6ab9
jordypabon <jordypabon723@gmail.com>
2025-11-01 23:00:42 -0400

    added variables

--- hangman.js
+++ hangman.js
@@ -1,13 +1,38 @@
-var POSSIBLE_WORDS = ["obdurate", "versatimilitude", "defenestrate", "obsequious", "dissonant","toady","idempotent"]
+var POSSIBLE_WORDS = ["obdurate", "versimilitude", "defenestrate", "obsequious", "dissonant","toady","idempotent"]
 var word = '';
+var guesses = '';
 
 function newGame(){
-    var randomIndex = parseInt(Math.random()) * POSSIBLE_WORDS.length;
+    var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+    word = POSSIBLE_WORDS[randomIndex];
 
-    var clueString = '';
-    for(var i = 0; i < word.length; i++){
-        clueString += '_ ';
-    }
-    var clue = document.getElementById('clue');
-    clue.innerHTML = clueString
+    guesses = "";
+    updatePage();
+}
+function guessLetter() {
+
+    var input = document.getElementById("guess");
+    var letter = input.value;
+    guesses += letter;
+
+    updatePage();
+}
+
+function updatePage() {
+    var clueString = "";
+     for (var i = 0; i < word.length; i++) {
+        clueString += "_ ";
+        var currentLetter = word.charAt(i);
+        if(guesses.indexOf(currentLetter) >= 0){
+            clueString += currentLetter + " ";
+        } else {
+            clueString += " ";
+        }
+           
+        }
+         var clue = document.getElementById("clue");
+    clue.innerHTML = clueString;
+
+       var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guessed Letters: " + guesses;
 }
