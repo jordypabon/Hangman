66451200efc7e7bf05b4248800fc1e9e480ff71e
jordypabon <jordypabon723@gmail.com>
2025-11-01 23:22:25 -0400

    max guess count

--- hangman.js
+++ hangman.js
@@ -1,18 +1,25 @@
-var POSSIBLE_WORDS = ["obdurate", "versimilitude", "defenestrate", "obsequious", "dissonant","toady","idempotent"]
+var POSSIBLE_WORDS = ["obdurate", "versimilitude", "defenestrate", "obsequious", "dissonant","toady","idempotent"];
+var MAX_GUESSES = 6;
 var word = '';
 var guesses = '';
+var guess_count = MAX_GUESSES;
 
 function newGame(){
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
 
     guesses = "";
+    guess_count = MAX_GUESSES
     updatePage();
 }
 function guessLetter() {
 
     var input = document.getElementById("guess");
     var letter = input.value;
+    if(word.indexOf(letter)< 0)
+    {
+        guess_count--;
+    }
     guesses += letter;
 
     updatePage();
@@ -35,4 +42,8 @@ function updatePage() {
 
        var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed Letters: " + guesses;
+
+ document.getElementById('hangmanImage');
+ Image.src = 'images/hangman' + guess_count + '.gif';
+
 }
