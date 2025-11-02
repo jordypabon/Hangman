db8af037447b55d77dd7db6c43e555d996e2bed0
jordypabon <jordypabon723@gmail.com>
2025-11-01 22:06:32 -0400

    possible words and css

---
+++ hangman.js
@@ -0,0 +1,3 @@
+var POSSIBLE_WORDS = ["obdurate", "versatimilitude", "defenestrate", "obsequious", "dissonant","toady","idempotent"]
+var randomIndex = parseInt(Math.random()) * POSSIBLE_WORDS.length;
+var word = POSSIBLE_WORDS[randomIndex]
