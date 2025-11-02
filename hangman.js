3a8468d0021dbc46ece17134c1f7ea6f603c2cca
jordypabon <jordypabon723@gmail.com>
2025-11-01 22:38:24 -0400

    first newGame additions

--- hangman.js
+++ hangman.js
@@ -1,3 +1,13 @@
 var POSSIBLE_WORDS = ["obdurate", "versatimilitude", "defenestrate", "obsequious", "dissonant","toady","idempotent"]
-var randomIndex = parseInt(Math.random()) * POSSIBLE_WORDS.length;
-var word = POSSIBLE_WORDS[randomIndex]
+var word = '';
+
+function newGame(){
+    var randomIndex = parseInt(Math.random()) * POSSIBLE_WORDS.length;
+
+    var clueString = '';
+    for(var i = 0; i < word.length; i++){
+        clueString += '_ ';
+    }
+    var clue = document.getElementById('clue');
+    clue.innerHTML = clueString
+}
